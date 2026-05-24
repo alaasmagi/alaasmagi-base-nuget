363377b2616bedfdf08719db6d023034c4913f07
Aleksander Laasmägi <alaasmagi@gmail.com>
2026-05-24 18:18:51 +0300

    Fixed a bug related to BaseEntityUser [NUGET_PUBLISH]

--- Base.Domain/BaseEntityUser.cs
+++ Base.Domain/BaseEntityUser.cs
@@ -4,22 +4,31 @@ using Base.Contracts.Domain;
 namespace Base.Domain;
 
 /// <summary>
-/// Provides a base entity implementation that stores a <see cref="Guid"/> user identifier.
+/// Provides a base entity implementation that stores a <see cref="Guid"/> entity identifier and a <see cref="Guid"/> user identifier.
 /// </summary>
-public abstract class BaseEntityUser : BaseEntityUser<Guid>
+public abstract class BaseEntityUser : BaseEntityUser<Guid, Guid>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BaseEntityUser"/> class with a new identifier value.
+    /// </summary>
+    protected BaseEntityUser()
+    {
+        Id = Guid.NewGuid();
+    }
 }
 
 /// <summary>
-/// Provides a base entity implementation that stores a strongly typed user identifier.
+/// Provides a base entity implementation that stores a strongly typed entity identifier and user identifier.
 /// </summary>
-/// <typeparam name="TKey">The identifier type of the user.</typeparam>
-public abstract class BaseEntityUser<TKey> : IBaseEntityUserId<TKey>
+/// <typeparam name="TKey">The identifier type of the entity.</typeparam>
+/// <typeparam name="TUserKey">The identifier type of the user.</typeparam>
+public abstract class BaseEntityUser<TKey, TUserKey> : BaseEntity<TKey>, IBaseEntityUserId<TUserKey>
     where TKey : IEquatable<TKey>
+    where TUserKey : IEquatable<TUserKey>
 {
     /// <summary>
     /// Gets or sets the identifier of the user associated with the entity.
     /// </summary>
     [Required]
-    public virtual TKey UserId { get; set; } = default!;
+    public virtual TUserKey UserId { get; set; } = default!;
 }
